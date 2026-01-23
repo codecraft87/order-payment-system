d7ce7f7dff0aaac71a58fde41c1791650a57680e
Mahesh <maheshk.mca@gmail.com>
2026-01-23 16:46:39 +0530

    Initial commit - Spring Boot base structure

---
+++ src/test/java/org/orderpaymentsystem/demo/OrderpaymentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orderpaymentsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderpaymentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
